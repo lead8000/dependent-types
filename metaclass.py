e6564361f99c3120aa1a4e77fd0ce23f788da236
leandro-driguez <leandro__rodriguez@outlook.com>
2022-10-12 15:15:26 -0400

    feat(metaclass): new approach to solving the problem, with metaclasses

--- metaclass.py
+++ metaclass.py
@@ -2,25 +2,26 @@
 from typing import Generic, List, TypeVar
 
 
-T = TypeVar('T')
-K = TypeVar('K')
-class Add(Generic[T, K]):
-	pass
+T = TypeVar('T'); K = TypeVar('K')
+class Add(Generic[T, K]): pass
 
 class Annotated(type):
 	def __add__(cls, otherCls):			
 		return Add[cls, otherCls]
 
-class N(metaclass=Annotated):
-	pass
 
-class M(metaclass=Annotated):
-	pass
+class N(metaclass=Annotated): pass
+class M(metaclass=Annotated): pass
 
-def add_something(l: List[N]) -> List[N + 1]:
-    l.append("yes")
-    return l
+def add_something(n: List[N]) -> List[N + 1]:
+    n.append("yes")
+    return n
+
+def create_list(n: N, m: M) -> List[N + M]:
+	return n * [0] + m * [1]
 
 assert add_something(['no', 'no']) == ['no', 'no', 'yes']
-
 print(add_something.__annotations__)
+
+assert create_list(4,5) == [0, 0, 0, 0, 1, 1, 1, 1, 1]
+print(create_list.__annotations__)
