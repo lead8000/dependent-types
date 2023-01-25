b37d9e908330b3ebc2f57b63a4a3ee9dea29895e
leandro-driguez <leandro__rodriguez@outlook.com>
2023-01-24 23:47:35 -0500

    feat(dev)

--- test2.py
+++ test2.py
@@ -8,7 +8,10 @@ m = Matrix([[23,3,5,43],[95,5],[93,12],[55,53]])
 N = GetAttr(Matrix, 'amount_rows')
 M = GetAttr(Matrix, 'amount_cols')
 
-# if isinstance(m, Matrix[ N, 4 | ( (N > 2 * M) & (N <= M + 1) | (M != 4) ) ]):
+if isinstance(m, Matrix[ N, 4 | ( (N > 2 * M) & (N <= M + 1) | (M != 4) ) ]):
+    print('YEESSSS')
+else:
+    print('NOOOOOO')
 
 # expr = (2*N > M) | ( (M != 1) & (N % 2 == 1) ) | (M > 50)
 
@@ -16,10 +19,7 @@ class A:...
 class B(A):...
 assert issubclass(B, A)
 
-if issubclass(Matrix[ N | ( (N < 100) & ( N > 50) ) ], Matrix[ N | ( N > 50 ) ]):
-    print('YEESSSS')
-else:
-    print('NOOOOOO')
+# if issubclass(Matrix[ N | ( (N < 100) & ( N > 50) ) ], Matrix[ N | ( N > 50 ) ]):
 
 # expr = N > 50
 # ctx = { 'vars': {}, 'ranges': {} }
